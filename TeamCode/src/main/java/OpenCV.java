e9777e7ee049f0154bf713a1d28b63cf4d88f15e
manow <26hossam@elmbrookstudents.org>
2025-10-24 18:40:09 -0500

    OpenCV

---
+++ TeamCode/src/main/java/OpenCV.java
@@ -0,0 +1,40 @@
+import android.util.Size;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+
+public class OpenCV extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+        AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
+                .setDrawAxes(true)
+                .setDrawCubeProjection(true)
+                .setDrawTagID(true)
+                .setDrawTagOutline(true)
+                .build();
+
+        VisionPortal visionPortal = new VisionPortal.Builder()
+                .addProcessor(tagProcessor)
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .setCameraResolution(new Size(640, 480))
+                .build();
+
+        waitForStart();
+
+        while (!isStopRequested() && opModeIsActive()) {
+
+            if (tagProcessor.getDetections().size() > 0) {
+                AprilTagDetection tag = tagProcessor.getDetections().get(0);
+            }
+
+
+        }
+    }
+}
