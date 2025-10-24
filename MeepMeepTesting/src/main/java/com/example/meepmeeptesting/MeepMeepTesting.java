2db51fb5cd1eef5e4497ec1a5852580c7d6d1bfd
FTCPackapunch <ftcpackapunch@gmail.com>
2025-10-23 23:21:58 -0500

    Updated meep meep testing class

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,20 +1,34 @@
 package com.example.meepmeeptesting;
-//import com.acmerobotics.roadrunner.geometry.Pose2d;
-//import com.noahbres.meep.MeepMeep;
-//import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
-//
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
 public class MeepMeepTesting {
-//    public static void main(String[] args) {
-//        MeepMeep mm = new MeepMeep(800);
-//        .setBackground(MeepMeep).Background.FIELD_ULTIMATE_GOAL_DARK)
-//        .setTheme(new ColorSchemeRedDark());
-//        .setBackgroundAlpha(1f)
-//                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-//                .followTrajectorySequence(drive->
-//                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-//                                .forward(30)
-//                                .build()
-//                )
-//                .start();
-//    }
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
+                .lineToX(30)
+                .turn(Math.toRadians(90))
+                .lineToY(30)
+                .turn(Math.toRadians(90))
+                .lineToX(0)
+                .turn(Math.toRadians(90))
+                .lineToY(0)
+                .turn(Math.toRadians(90))
+                .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
 }
