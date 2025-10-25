47187358d4ee7e226261d1e2d558716e16f9451c
manow <26hossam@elmbrookstudents.org>
2025-10-24 19:32:31 -0500

    final OpenCV

--- TeamCode/src/main/java/OpenCV.java
+++ TeamCode/src/main/java/OpenCV.java
@@ -1,13 +1,14 @@
 import android.util.Size;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
-
+@TeleOp(name = "TestOpenCV", group = "Sensor")
 public class OpenCV extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
