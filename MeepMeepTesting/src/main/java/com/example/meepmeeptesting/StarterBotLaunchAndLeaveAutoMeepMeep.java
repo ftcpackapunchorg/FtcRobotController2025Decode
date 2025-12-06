c4aee68452de55a42bf093317b049443762dc244
FTCPackapunch <ftcpackapunch@gmail.com>
2025-12-06 11:10:37 -0600

    CADBot draft changes and starterbot misc changes

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/StarterBotLaunchAndLeaveAutoMeepMeep.java
@@ -0,0 +1,59 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class StarterBotLaunchAndLeaveAutoMeepMeep {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        boolean blueAlliance = false;
+
+        Pose2d initialPose;
+
+        Pose2d blueAllianceStartPose = new Pose2d(-48, -48, Math.toRadians(-135));
+
+        Pose2d redAllianceStartPose = new Pose2d(-48, 48, Math.toRadians(135));
+
+        if(blueAlliance) {
+
+            initialPose = blueAllianceStartPose;
+        } else {
+            initialPose = redAllianceStartPose;
+        }
+
+        boolean isReversed = true;
+
+        System.out.println("Initial Pose : " + initialPose.toString());
+
+        myBot.runAction(myBot.getDrive().actionBuilder(initialPose)
+                        .waitSeconds(1)
+                        .strafeTo(new Vector2d(-24, -24))
+                        .turn(Math.toRadians(-55))
+                        .strafeTo(new Vector2d(58, -24))
+                        .strafeTo(new Vector2d(58, -33 ))
+//                .lineToX(-30)
+//                .turn(Math.toRadians(90))
+//                .lineToY(-30)
+//                .turn(Math.toRadians(90))
+//                .lineToX(0)
+//                .turn(Math.toRadians(90))
+//                .lineToY(0)
+//                .turn(Math.toRadians(90))
+                .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cadbot/mechanisms/CADBotIntakeFeederMechanism.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cadbot/mechanisms/CADBotIntakeFeederMechanism.java
@@ -1,28 +1,27 @@
 package org.firstinspires.ftc.teamcode.cadbot.mechanisms;
 
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.cadbot.utils.CADBotConstants;
 
 public final class CADBotIntakeFeederMechanism {
 
-    public final CRServo leftIntakeFeeder, rightIntakeFeeder;
+    public final Servo leftIntakeFeeder, rightIntakeFeeder;
     final double STOP_SPEED = 0.0;
     final double MAX_SPEED = 0.5;
 
     public CADBotIntakeFeederMechanism(HardwareMap hardwareMap, Telemetry telemetry) {
 
-        leftIntakeFeeder = hardwareMap.get(CRServo.class, CADBotConstants.LEFT_INTAKE_CR_SERVO_NAME);
-        rightIntakeFeeder = hardwareMap.get(CRServo.class, CADBotConstants.RIGHT_INTAKE_CR_SERVO_NAME);
+        leftIntakeFeeder = hardwareMap.get(Servo.class, CADBotConstants.LEFT_INTAKE_CR_SERVO_NAME);
+        rightIntakeFeeder = hardwareMap.get(Servo.class, CADBotConstants.RIGHT_INTAKE_CR_SERVO_NAME);
 
         /*
          * Much like our drivetrain motors, we set the left feeder servo to reverse so that they
          * both work to feed the ball into the robot.
          */
-        leftIntakeFeeder.setDirection(DcMotorSimple.Direction.REVERSE);
+//        leftIntakeFeeder.setDirection(DcMotorSimple.Direction.REVERSE);
 
         /*
          * set Feeders to an initial value to initialize the servo controller
@@ -31,13 +30,13 @@ public final class CADBotIntakeFeederMechanism {
     }
 
     public void startIntakeFeeders() {
-        leftIntakeFeeder.setPower(MAX_SPEED);
-        rightIntakeFeeder.setPower(MAX_SPEED);
+        leftIntakeFeeder.setPosition(MAX_SPEED);
+        rightIntakeFeeder.setPosition(MAX_SPEED);
     }
 
     public void stopIntakeFeeders() {
-        leftIntakeFeeder.setPower(STOP_SPEED);
-        rightIntakeFeeder.setPower(STOP_SPEED);
+        leftIntakeFeeder.setPosition(STOP_SPEED);
+        rightIntakeFeeder.setPosition(STOP_SPEED);
     }
 
 }
