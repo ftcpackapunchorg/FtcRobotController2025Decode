983a7e7f8787f0ba4258706472914ec728c20cc1
arshan <arshanarvind9@gmail.com>
2025-12-19 18:37:47 -0600

    test opmode for distance sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testbench/testOpModes/AprilTagLimelight/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testbench/testOpModes/AprilTagLimelight/Test.java
@@ -41,7 +41,7 @@ public class Test extends OpMode {
             Telemetry.Item yaw = telemetry.addData("Yaw", botpose.getOrientation().getYaw());
 
         }
-        // jertoertjut
+        // jertoertjutgdf
 }
 }
 
